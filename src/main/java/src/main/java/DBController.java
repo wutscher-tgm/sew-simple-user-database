865fa0e756e4530ddef3bd74ee19a2e1eab7d2a9
rwutscher <ricwu01@gmail.com>
2019-01-22 14:16:51 +0100

    added keybinds for deleting

--- src/main/java/src/main/java/DBController.java
+++ src/main/java/src/main/java/DBController.java
@@ -4,42 +4,30 @@ import com.jfoenix.controls.*;
 import com.jfoenix.controls.cells.editors.TextFieldEditorBuilder;
 import com.jfoenix.controls.cells.editors.base.GenericEditableTreeTableCell;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-import com.squareup.okhttp.MediaType;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
+
+import io.datafx.controller.ViewController;
+
+import javafx.scene.input.KeyCode;
+import org.json.*;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TreeTableCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.control.TreeTableColumn;
 
 import java.io.IOException;
-import java.security.SecureRandom;
-import java.util.Random;
-import io.datafx.controller.ViewController;
-
-import javax.annotation.PostConstruct;
 import java.util.function.Function;
 
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import org.json.*;
-
-import main.java.Connector;
 
 @ViewController(value = "/fxml/ui/TreeTableView.fxml", title = "Material Design Example")
 public class DBController {
 
-    //PSQL Variables
     @FXML
     private JFXTreeTableView<Student> editableTreeTableView;
 
@@ -47,30 +35,19 @@ public class DBController {
     private JFXTreeTableColumn<Student, ImageView> pictureColumn;
 
     @FXML
-    private JFXTreeTableColumn<Student, String> usernameColumn;
+    private JFXTreeTableColumn<Student, String> usernameColumn,emailColumn;
 
     @FXML
-    private JFXTreeTableColumn<Student, String> emailColumn;
+    private JFXTextField usernameCreate,emailCreate,linkCreate;
 
     @FXML
-    private JFXButton testButton;
+    private JFXDialog dialog;
 
     @FXML
-    private Label errorLabel;
+    private StackPane root;
 
     @FXML
-    private JFXTextField usernameCreate;
-    @FXML
-    private JFXTextField emailCreate;
-    @FXML
-    private JFXTextField linkCreate;
-
-    private final String[] names = {"Morley", "Scott", "Kruger", "Lain",
-            "Kennedy", "Gawron", "Han", "Hall", "Aydogdu", "Grace",
-            "Spiers", "Perera", "Smith", "Connoly",
-            "Sokolowski", "Chaow", "James", "June",};
-
-    private final Random random = new SecureRandom();
+    private Label errorDialogText;
 
     private Connector connector = new Connector("http://localhost:5000");
 
@@ -80,29 +57,51 @@ public class DBController {
     }
 
     @FXML
+    public void onMessageClose(){
+        dialog.close();
+    }
+
+    private void showErrorWindow(String content){
+        errorDialogText.setText(content);
+        dialog.setTransitionType(JFXDialog.DialogTransition.CENTER);
+        dialog.setDialogContainer(root);
+        dialog.show();
+    }
+
+    @FXML
+    private void onReload(){
+        setupEditableTableView();
+        System.out.println("Data loaded");
+    }
+
+    @FXML
     private void onSend(){
         try {
             this.connector.create(emailCreate.getText(), usernameCreate.getText(), linkCreate.getText());
             this.setupEditableTableView();
         } catch (IOException e) {
-            this.errorLabel.setText("Could not create User");
+            showErrorWindow("Could not create user due to connection issues");
             e.printStackTrace();
         }
     }
 
-    @PostConstruct
-    public void init() {
-        setupEditableTableView();
-    }
-
     @FXML
     public void onDelete(){
-        Student s = editableTreeTableView.getSelectionModel().getSelectedItem().getValue();
+        try{
+            Student s = editableTreeTableView.getSelectionModel().getSelectedItem().getValue();
+            this.deleteStudent(s);
+        }catch(NullPointerException e){
+            showErrorWindow("Please select the user you want to delete in the table above.");
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteStudent(Student s){
         try {
             this.connector.delete(s.emailProperty().getValue());
             this.setupEditableTableView();
         } catch (IOException e) {
-            this.errorLabel.setText("Could not delete user due to connection issues");
+            showErrorWindow("Could not delete user due to connection issues");
             e.printStackTrace();
         }
     }
@@ -112,7 +111,6 @@ public class DBController {
         setupCellValueFactory(emailColumn, Student::emailProperty);
         //setupCellValueFactory(pictureColumn, Student::pictureProperty);
 
-        // add editors
         usernameColumn.setCellFactory((TreeTableColumn<Student, String> param) -> {
             return new GenericEditableTreeTableCell<>(
                     new TextFieldEditorBuilder());
@@ -127,7 +125,7 @@ public class DBController {
                 this.connector.update(email, username);
                 student.usernameProperty().set(username);
             } catch (IOException e) {
-                errorLabel.setText("UPDATING STUDENT FAILED");
+                showErrorWindow("Could not update user due to connection issues");
                 e.printStackTrace();
             }
         });
@@ -136,37 +134,18 @@ public class DBController {
             return new GenericEditableTreeTableCell<>(
                     new TextFieldEditorBuilder());
         });
-        /*emailColumn.setOnEditCommit((TreeTableColumn.CellEditEvent<Student, String> t) -> {
-            t.getTreeTableView()
-                    .getTreeItem(t.getTreeTablePosition()
-                            .getRow())
-                    .getValue().emailProperty().set(t.getNewValue());
-            System.out.println("Edited Email");
-        });*/
         emailColumn.setEditable(false);
 
         editableTreeTableView.setOnKeyReleased(event -> {
-            Object o = event.getSource();
-            System.out.println(editableTreeTableView.getSelectionModel().getSelectedItem());
-        });
-
-
-
-        editableTreeTableView.setOnContextMenuRequested(event -> {
-            Object o = event.getPickResult().getIntersectedNode().getParent();
-
-            JFXTreeTableRow row = null;
-
-            if(o instanceof JFXTreeTableRow){
-                row = (JFXTreeTableRow) o;
-            }else if(o instanceof GenericEditableTreeTableCell){
-                row = (JFXTreeTableRow)((GenericEditableTreeTableCell) o).getParent();
-            }else{
-                row = (JFXTreeTableRow) ((javafx.scene.Group)o).getChildren().toArray()[0];
+            if(event.getCode() == KeyCode.DELETE || event.getCode() == KeyCode.BACK_SPACE){
+                try{
+                    Student s = editableTreeTableView.getSelectionModel().getSelectedItem().getValue();
+                    this.deleteStudent(s);
+                }catch(NullPointerException e){
+                    showErrorWindow("Please select the user you want to delete in the table above.");
+                    e.printStackTrace();
+                }
             }
-
-            Student student = (Student) row.getItem();
-            System.out.println(student.emailProperty().getValue());
         });
 
         //pictureColumn.setCellValueFactory(param -> param.getValue().getValue().picture);
@@ -193,7 +172,6 @@ public class DBController {
     }
 
     private ObservableList<Student> getStudents() {
-
         try{
             JSONArray arr = new JSONArray(connector.get());
             final ObservableList<Student> dummyData = FXCollections.observableArrayList();
@@ -201,24 +179,20 @@ public class DBController {
                 dummyData.add(new Student((String)((JSONObject) a).get("email"), (String)((JSONObject) a).get("username")/*, new ImageView()*/));
             }
             return dummyData;
-        }catch(Exception e){
-            errorLabel.setText("LOADING STUDENT DATA FAILED");
+        }catch(IOException e){
+            showErrorWindow("Could not load users due to connection issues");
             e.printStackTrace();
+            return null;
         }
-
-
-        return null;
     }
 
     static final class Student extends RecursiveTreeObject<Student> {
         final StringProperty username;
         final StringProperty email;
-        //final ImageView picture;
 
-        Student(String email, String username/*, ImageView picture*/) {
+        Student(String email, String username) {
             this.email = new SimpleStringProperty(email);
             this.username = new SimpleStringProperty(username);
-            /*this.picture = picture;*/
         }
 
         StringProperty usernameProperty() {
@@ -228,34 +202,5 @@ public class DBController {
         StringProperty emailProperty() {
             return email;
         }
-
-        /*ImageView pictureProperty() {
-            return picture;
-        }*/
-    }
-
-    @FXML
-    private void test(){
-        /*ObservableList<Student> students = FXCollections.observableArrayList();
-        students.add(new Student("asddssaddas", "21"));
-        students.add(new Student("asddsasadsad", "28"));
-        final TreeItem<Student> root = new RecursiveTreeItem<>(students, RecursiveTreeObject::getChildren);
-        editableTreeTableView.setRoot(root);
-        editableTreeTableView.setShowRoot(false);
-        */
-
-        setupEditableTableView();
-        System.out.println("Data loaded");
-    }
-
-
-
-    private ChangeListener<String> setupSearchField(final JFXTreeTableView<Student> tableView) {
-        return (o, oldVal, newVal) ->
-                tableView.setPredicate(studentProp -> {
-                    final Student student = studentProp.getValue();
-                    return student.username.get().contains(newVal)
-                            || student.email.get().contains(newVal);
-                });
     }
 }
