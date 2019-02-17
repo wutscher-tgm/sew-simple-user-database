5d90cef4ca769e3eebfc414ecea70c77fd6900c0
Richard Wutscher <me@rwutscher.com>
2019-02-17 22:51:34 +0100

    implemented basic authentication using argon2-hashed password

--- src/main/python/server/main.py
+++ src/main/python/server/main.py
@@ -3,9 +3,13 @@ import urllib
 import os
 
 import werkzeug
+import authdigest
 from flask import Flask
+import flask
 from flask_restful import Resource, Api, reqparse
 import json
+from argon2 import PasswordHasher
+ph = PasswordHasher()
 
 true = True
 false = False
@@ -17,22 +21,7 @@ api = Api(app)
 from flask_cors import CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-
-# AUTH
-from functools import wraps
-
-class FlaskRealmDigestDB(werkzeug.contrib.authdigest.RealmDigestDB):
-    def requires_auth(self, f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            request = flask.request
-            if not self.isAuthenticated(request):
-                return self.challenge()
-
-            return f(*args, **kwargs)
-
-        return decorated
-# AUTH
+tokens = []
 
 class DB:
     def __init__(self, location):
@@ -70,6 +59,15 @@ class DB:
                     return element
         else:
             return self.__db
+    
+    def getUser(self, email=null):
+        if email != null:
+            for element in self.__db:
+                if element['email'] == email:
+                    return element
+        else:
+            return null
+    
 
     def update(self, email, username=null, picture=null):
         if email != null:
@@ -90,14 +88,65 @@ class DB:
         self.__db.remove(entry)
         self.save(self.__db)
 
+# AUTH
+from functools import wraps
 
-from flask import request, session
+class FlaskRealmDigestDB():#(authdigest.RealmDigestDB):
+    def __init__(self, db):
+        self.db = db
+
+    def requires_auth(self, f):
+        """@wraps(f)
+        def decorated(*args, **kwargs):
+            request = flask.request
+            print("Authenticated:  " + str(self.isAuthenticated(request).authenticated))
+            if not self.isAuthenticated(request).authenticated:
+                return self.challenge()
+            #print('Wrapping now')
+            return f(*args, **kwargs)
+        """
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            if 'tok' in flask.request.cookies:
+                token = flask.request.cookies['tok']
+                for element in tokens:
+                    if element['token'] == token:
+                        return f(*args, **kwargs)
+                return 'user not found'
+            else:
+                print('no cookie')
+                return 'no cookie'
+
+        return decorated
+# AUTH
+import random
+import string
+
+from flask import request, session  
 class Schueler(Resource):
 
+    authDB = FlaskRealmDigestDB(0)
+    #('MyAuthRealm', DB("db.json"), algorithm="sha256")
+    #authDB.add_user('admin'.encode('utf-8'), 'test'.encode('utf-8'))
     def __init__(self):
-        authDB = FlaskRealmDigestDB('MyAuthRealm')
-        authDB.add_user('admin', 'test')
         self.__db = DB("db.json")
+        #authDB = FlaskRealmDigestDB(self.__db)
+    
+    @authDB.requires_auth
+    def get(self):
+        #print(request.authorization.username)
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str)
+        parser.add_argument('username', type=str)
+        email = parser.parse_args().email
+        username = parser.parse_args().username
+
+        if email != null:
+            data = self.__db.get(email=email)
+            if(data == None): return "user not found", 404
+            return data, 200
+        else:
+            return self.__db.get()
     
     def post(self):
         
@@ -106,6 +155,7 @@ class Schueler(Resource):
         parser.add_argument('email', type=str)
         parser.add_argument('username', type=str)
         parser.add_argument('pictureLink', type=str)
+        parser.add_argument('password', type=str)
         parser.add_argument('picture', type=werkzeug.datastructures.FileStorage, location='files')
 
         # Loading arguments into easily usable variables
@@ -113,6 +163,7 @@ class Schueler(Resource):
         pictureLink = parser.parse_args().pictureLink
         username = parser.parse_args().username
         email = parser.parse_args().email
+        password = parser.parse_args().password
         picture = null
 
         # Checking if arguments are valid
@@ -132,26 +183,11 @@ class Schueler(Resource):
             [
                 {"email": email,
                  "username": username,
-                 "picture": picture
+                 "picture": picture,
+                 "password": ph.hash(password)
                  }
             ]
         )
-
-    @authDB.requires_auth
-    def get(self):
-        print(request.authorization.username)
-        parser = reqparse.RequestParser()
-        parser.add_argument('email', type=str)
-        parser.add_argument('username', type=str)
-        email = parser.parse_args().email
-        username = parser.parse_args().username
-
-        if email != null:
-            data = self.__db.get(email=email)
-            if(data == None): return "user not found", 404
-            return data, 200
-        else:
-            return self.__db.get()
 
     def patch(self):
         # Getting arguments from request
@@ -192,8 +228,34 @@ class Schueler(Resource):
         if result == None:
             return "user not found", 404
 
+    def options(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('email', type=str)
+        parser.add_argument('password', type=str)
+        email = parser.parse_args().email
+        password = parser.parse_args().password
+
+        user = self.__db.getUser(email)
+
+        if user != None:
+            try:
+                if ph.verify(user['password'], password):
+                    token = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+                    tokens.append({
+                        'token': token,
+                        'user': user['password']
+                    })
+                    return 'success', {'Set-Cookie': 'tok='+token}
+                else:
+                    return 'not valid'
+            except:
+                return 'not valid'
+        else:
+            return 'not valid'
+
+
 api.add_resource(Schueler, '/students')
 
-print(__name__)
+
 if __name__ == '__main__':
     app.run(debug=true, host='0.0.0.0')
