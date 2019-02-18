2e3504034f5153c175a9754013bb05719143a1d2
Richard Wutscher <me@rwutscher.com>
2019-02-18 15:35:14 +0100

    basic digest login implemented

--- src/main/python/server/main.py
+++ src/main/python/server/main.py
@@ -9,6 +9,7 @@ from flask import Flask
 import flask
 from flask_restful import Resource, Api, reqparse
 import json
+from flask_httpauth import HTTPDigestAuth
 from argon2 import PasswordHasher
 ph = PasswordHasher()
 
@@ -19,10 +20,22 @@ null = None
 app = Flask('SimpleUserDatabase')
 api = Api(app)
 
+app.config['SECRET_KEY'] = 'secret key here'
+auth = HTTPDigestAuth()
+
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 from flask_cors import CORS
 CORS(app, resources={r"/*": {"origins": "*"}})
-
-tokens = []
 
 class DB:
     def __init__(self, location):
@@ -91,53 +104,17 @@ class DB:
         self.__db.remove(entry)
         self.save(self.__db)
 
-# AUTH
-from functools import wraps
-
-class FlaskRealmDigestDB():#(authdigest.RealmDigestDB):
-    def __init__(self, db):
-        self.db = db
-
-    def requires_auth(self, f):
-        """@wraps(f)
-        def decorated(*args, **kwargs):
-            request = flask.request
-            print("Authenticated:  " + str(self.isAuthenticated(request).authenticated))
-            if not self.isAuthenticated(request).authenticated:
-                return self.challenge()
-            #print('Wrapping now')
-            return f(*args, **kwargs)
-        """
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            if app.testing:
-                return f(*args, **kwargs)
-            if 'tok' in flask.request.cookies:
-                token = flask.request.cookies['tok']
-                for element in tokens:
-                    if element['token'] == token:
-                        return f(*args, **kwargs)
-                return 'user not found'
-            else:
-                print('no cookie')
-                return 'no cookie'
-
-        return decorated
-# AUTH
-import random
-import string
-
 from flask import request, session  
 class Schueler(Resource):
 
-    authDB = FlaskRealmDigestDB(0)
+    #authDB = FlaskRealmDigestDB(0)
     #('MyAuthRealm', DB("db.json"), algorithm="sha256")
     #authDB.add_user('admin'.encode('utf-8'), 'test'.encode('utf-8'))
     def __init__(self):
         self.__db = DB("db.json")
         #authDB = FlaskRealmDigestDB(self.__db)
     
-    @authDB.requires_auth
+    @auth.login_required
     def get(self):
         #print(request.authorization.username)
         parser = reqparse.RequestParser()
@@ -234,32 +211,6 @@ class Schueler(Resource):
         result = self.__db.delete(email)
         if result == None:
             return "user not found", 404
-
-    def options(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('email', type=str)
-        parser.add_argument('password', type=str)
-        email = parser.parse_args().email
-        password = parser.parse_args().password
-
-        user = self.__db.getUser(email)
-
-        if user != None:
-            try:
-                if ph.verify(user['password'], password):
-                    token = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
-                    #TODO: check that token doesn't exist
-                    tokens.append({
-                        'token': token,
-                        'user': user['password']
-                    })
-                    return 'success', {'Set-Cookie': 'tok='+token}
-                else:
-                    return 'not valid'
-            except:
-                return 'not valid'
-        else:
-            return 'not valid'
 
 
 api.add_resource(Schueler, '/students')
